0a16014e086c636373b754f68639318231141870
kamilcyan <kamil.lucjanek@wp.pl>
2019-05-15 19:03:14 +0200

    to będzie gut

--- lista/main.cpp
+++ lista/main.cpp
@@ -16,34 +16,70 @@ int rysujMenu()
     cout << "Wybierz opcje:" << endl;
     cout << "Dodaj  rosline\t1" << endl;
     cout << "Skasuj  rosline\t2" << endl;
+    cout << "Pokaz liste\t3" << endl;
     cout << "Wyjdz\t0" << endl;
     cin >> wybor;
     return wybor;
 }
 
+void Dodaj(Rosliny *&glowa)
+{
+    Rosliny *aktualny, *poprzedni;
+    poprzedni = aktualny;
+
+
+    aktualny = new Rosliny;
+    string nazwa;
+    cout << "Podaj nazwe:" << endl;
+    cin >> nazwa;
+
+
+    aktualny->nazwa = nazwa;
+    aktualny->next = NULL;
+    if (poprzedni != NULL)
+        poprzedni->next = aktualny;
+    else
+        glowa = aktualny;
+
+
+}
+
+void Usun(Rosliny *&glowa)
+{
+
+}
+
+void Rysuj()
+{
+
+}
+
 int main()
 {
 
-    Rosliny *glowa, *aktualny, *poprzedni, *tmp;
+    Rosliny *glowa;
 
-    string nazwa;
+    glowa = NULL;
 
-    rysujMenu();
-
-    while(wybor==1){
-        poprzedni = aktualny;
-        cout << "Podaj nazwe:" << endl;
-        cin >> nazwa;
-        aktualny = new Rosliny;
-        aktualny->nazwa = nazwa;
-        aktualny->next = NULL;
-        if (poprzedni != NULL)
-	      poprzedni->next = aktualny;
-	    else
-	      glowa = aktualny;
-        rysujMenu();
+    int wybor=1;
+    while(wybor)
+    {
+        wybor =rysujMenu();
+        switch(wybor)
+        {
+        case 1:
+            Dodaj(glowa);
+            break;
+        case 2:
+            Usun(glowa);
+            break;
+        case 3:
+            Rysuj();
+            break;
+        default:
+            return 0;
+        }
     }
-
 
     return 0;
 }
