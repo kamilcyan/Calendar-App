3ea49503f698b06c0c627c63ef03708e0b4c72c5
kamilcyan <kamil.lucjanek@wp.pl>
2019-07-06 20:26:42 +0200

    update

--- lista/Program.cs
+++ lista/Program.cs
@@ -51,7 +51,18 @@ namespace ConsoleApp1
                 Console.ForegroundColor = ConsoleColor.Gray;
             }
         }
-        
+        static class CircularLinkedList
+        {
+            public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
+            {
+                return current.Next ?? current.List.First;
+            }
+
+            public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
+            {
+                return current.Previous ?? current.List.Last;
+            }
+        }
         /*
         public class CircularLinkedList<T> : LinkedList<T>
         {
@@ -61,22 +72,34 @@ namespace ConsoleApp1
             }
         }
 
-        
+        public class CircularLinkedListEnumerator<T> : IEnumerator<T>
+        {
+            private LinkedListNode<T> _current;
+            public T Current => _current.Value;
+            object IEnumerator.Current => Current;
+
+            public CircularLinkedListEnumerator(LinkedList<T> list)
+            {
+                _current = list.First;
+            }
+
+            public bool MoveNext()
+            {
+                if (_current == null)
+                {
+                    return false;
+                }
+                _current = _current.Next ?? _current.List.First;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _current = _current.List.First;
+            }
+            public void Dispose() { }
+        }
 
         */
-    }
-    internal class CircularLinkedList
-    {
-        
-
-        public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
-        {
-            return current.Next ?? current.List.First;
-        }
-
-        public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
-        {
-            return current.Previous ?? current.List.Last;
-        }
     }
 }
