eb39aac6146d9038757dc3b63b375104055cca3e
kamilcyan <kamil.lucjanek@wp.pl>
2019-08-14 19:20:20 +0200

    próba

--- lista/Calendar/MonthWindow.xaml.cs
+++ lista/Calendar/MonthWindow.xaml.cs
@@ -42,59 +42,73 @@ namespace Calendar
         public MonthWindow() : this(DateTime.Now)
         { }
 
+        //private void Load()
+        //{
+        //    _DisplayMonth = _DisplayStartDate.Month;
+        //    _DisplayYear = _DisplayStartDate.Year;
+        //    sysCal = _cultureInfo.Calendar;
+        //    iDaysInMonth = sysCal.GetDaysInMonth(_DisplayStartDate.Year, _DisplayStartDate.Month);
+
+        //    NewStackPanel2.Children.Clear();
+
+        //    int fDay = FirstDayOfTheMonth();
+
+
+
+        //    for (int i = 0; i < 1; i++)
+        //    {
+        //        for (int j = 0; j < fDay - 1; j++)
+        //        {
+        //            TextBlock textBlock = new TextBlock();
+        //            textBlock.Background = Brushes.White;
+
+        //            Grid.SetRow(textBlock, 0);
+        //            Grid.SetColumn(textBlock, j);
+        //            NewStackPanel2.Children.Add(textBlock);
+        //        }
+
+        //        for (int j = fDay - 1; j < 7; j++)
+        //        {
+        //            var date = new DateTime(_DisplayYear, _DisplayMonth, dayOfFirstDay);
+        //            SingleDayPage singleDayPage = new SingleDayPage(date);
+
+        //            Grid.SetRow(singleDayPage, 0);
+        //            Grid.SetColumn(singleDayPage, j);
+        //            NewStackPanel2.Children.Add(singleDayPage);
+        //            dayOfFirstDay++;
+        //        }
+        //    }
+        //    for (int i = 1; i < 5; i++)
+        //    {
+        //        for (int j = 0; j < 7; j++)
+        //        {
+        //            var date = new DateTime(_DisplayYear, _DisplayMonth, dayOfFirstDay);
+
+        //            SingleDayPage singleDayPage = new SingleDayPage(date);
+
+        //            Grid.SetRow(singleDayPage, i);
+        //            Grid.SetColumn(singleDayPage, j);
+        //            NewStackPanel2.Children.Add(singleDayPage);
+
+        //            if (dayOfFirstDay >= iDaysInMonth)
+        //                return;
+
+        //            dayOfFirstDay++;
+        //        }
+        //    }
+        //}
         private void Load()
         {
-            _DisplayMonth = _DisplayStartDate.Month;
-            _DisplayYear = _DisplayStartDate.Year;
-            sysCal = _cultureInfo.Calendar;
-            iDaysInMonth = sysCal.GetDaysInMonth(_DisplayStartDate.Year, _DisplayStartDate.Month);
-
-            NewStackPanel2.Children.Clear();
-
-            int fDay = FirstDayOfTheMonth();
-
-
-
-            for (int i = 0; i < 1; i++)
+            DateTime firstDayOfMonth = _date.AddDays(-_date.Day);
+            var offset = (int)firstDayOfMonth.DayOfWeek - 1;
+            for (int i = 0; i < 35; i++)
             {
-                for (int j = 0; j < fDay - 1; j++)
-                {
-                    TextBlock textBlock = new TextBlock();
-                    textBlock.Background = Brushes.White;
+                var day = firstDayOfMonth.AddDays(i - offset);
+                SingleDayPage singleDayPage = new SingleDayPage(day.Date, day.Month == _date.Month);
 
-                    Grid.SetRow(textBlock, 0);
-                    Grid.SetColumn(textBlock, j);
-                    NewStackPanel2.Children.Add(textBlock);
-                }
-
-                for (int j = fDay - 1; j < 7; j++)
-                {
-                    var date = new DateTime(_DisplayYear, _DisplayMonth, dayOfFirstDay);
-                    SingleDayPage singleDayPage = new SingleDayPage(date);
-
-                    Grid.SetRow(singleDayPage, 0);
-                    Grid.SetColumn(singleDayPage, j);
-                    NewStackPanel2.Children.Add(singleDayPage);
-                    dayOfFirstDay++;
-                }
-            }
-            for (int i = 1; i < 5; i++)
-            {
-                for (int j = 0; j < 7; j++)
-                {
-                    var date = new DateTime(_DisplayYear, _DisplayMonth, dayOfFirstDay);
-
-                    SingleDayPage singleDayPage = new SingleDayPage(date);
-
-                    Grid.SetRow(singleDayPage, i);
-                    Grid.SetColumn(singleDayPage, j);
-                    NewStackPanel2.Children.Add(singleDayPage);
-
-                    if (dayOfFirstDay >= iDaysInMonth)
-                        return;
-
-                    dayOfFirstDay++;
-                }
+                Grid.SetRow(singleDayPage, i / 7);
+                Grid.SetColumn(singleDayPage, i % 7);
+                NewStackPanel2.Children.Add(singleDayPage);
             }
         }
         private int FirstDayOfTheMonth()
